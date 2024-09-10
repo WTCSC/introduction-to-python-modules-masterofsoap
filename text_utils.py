22a0bd39cdde4af819613553740459c18071fc7a
Mxsterofsoap <soapsgang@gmail.comgit config --global user.name Josiah>
2024-09-10 10:41:01 -0600

    Solution

--- text_utils.py
+++ text_utils.py
@@ -1,14 +1,14 @@
-def count_chars(text):
-    """
-    Count the number of characters in a text
-    """
+def count_chars(text): # Defines the character count variable
+    return len(text) # Returns the count of each character
 
-def count_words(text):
-    """
-    Count the number of words in a text
-    """
+def count_words(text): # Defines the word count variable
+    return len(text.split()) # Splits each sentence and returns the word count
 
-def count_sentences(text):
-    """
-    Count the number of sentences in a text
-    """
+def count_sentences(text): # Defines the sentence count variable
+    sentence_endings = '.?!' # Defines what ends a sentence
+    count = 0 # Sets up the count variable
+    for char in text: #                } \
+        if char in sentence_endings: # } --> Looks at each set of words and looks for ?, ., or ! then when it finds one, it adds one to the count.
+            count += 1 #               } /
+    
+    return count # Returns the count of sentences
